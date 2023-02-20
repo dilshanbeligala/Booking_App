d05b355eda60e36075a2193d7f2c9baab05c3fd3
dilshanbeligala <beligaladilshan@gmail.com>
2023-02-20 13:32:28 +0530

    property list done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import  PropertyList  from '../../components/propertyList/PropertyList'
 
 import "./home.css"
 
@@ -13,7 +14,7 @@ import "./home.css"
         <div className="homeContainer">
         <Featured/>
         <h1 className="homeTitle">Browse by property type</h1>
-        
+        <PropertyList/>
         </div>
         
     </div>
