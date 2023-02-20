e40f5ad6375ffb39ffa9afea605f4c5ec9c287c3
dilshanbeligala <beligaladilshan@gmail.com>
2023-02-20 20:18:00 +0530

    featured list done

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,52 @@
+import "./featuredProperties.css"
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+        <div className="fpItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/421853145.webp?k=140bfc6c54ee753d4a748ee7b5a86c00c988e6fc9bb340c87172ead66a3ea9d5&o=&s=1" alt="" className="fpImg" />
+        <span className="fpName">Aparthotel stare </span>
+        <span className="fpCity">Kandy</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excelent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/421853145.webp?k=140bfc6c54ee753d4a748ee7b5a86c00c988e6fc9bb340c87172ead66a3ea9d5&o=&s=1" alt="" className="fpImg" />
+        <span className="fpName">Aparthotel stare </span>
+        <span className="fpCity">Kandy</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excelent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/421853145.webp?k=140bfc6c54ee753d4a748ee7b5a86c00c988e6fc9bb340c87172ead66a3ea9d5&o=&s=1" alt="" className="fpImg" />
+        <span className="fpName">Aparthotel stare </span>
+        <span className="fpCity">Kandy</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excelent</span>
+        </div>
+        </div>
+        <div className="fpItem">
+        <img src="https://cf.bstatic.com/xdata/images/hotel/square600/421853145.webp?k=140bfc6c54ee753d4a748ee7b5a86c00c988e6fc9bb340c87172ead66a3ea9d5&o=&s=1" alt="" className="fpImg" />
+        <span className="fpName">Aparthotel stare </span>
+        <span className="fpCity">Kandy</span>
+        <span className="fpPrice">Starting from $120</span>
+        <div className="fpRating">
+            <button>8.9</button>
+            <span>Excelent</span>
+        </div>
+        </div>
+    
+    
+    </div>
+  )
+}
+
+export default FeaturedProperties
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import  PropertyList  from '../../components/propertyList/PropertyList'
@@ -15,6 +16,8 @@ import "./home.css"
         <Featured/>
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList/>
+        <h1 className="homeTitle">Home guests love</h1>
+        <FeaturedProperties/>
         </div>
         
     </div>
